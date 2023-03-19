15a064298d97f0a746df9c8ee08e4fd3fc45c0d3
Makar Istratov <imakar98@gmail.com>
2023-03-20 00:47:36 +0200

    Code improvments

--- 7. task_js_dom/DOM/script.js
+++ 7. task_js_dom/DOM/script.js
@@ -2,24 +2,36 @@ import data from "./data.js"
 
 const itemList = document.querySelector(".menu__list"),
 intro = document.querySelector(".intro"),
-carItem = document.createElement("div"),
-menuItem = document.querySelectorAll(".menu__item");
+carItem = document.createElement("div");
 
 carItem.classList.add(".car__content");
 intro.parentElement.append(carItem);
 carItem.style.display = "none";
 
+function chosenCar ({title, content, image}) {
+    return `
+    <h2 style="text-transform:uppercase;">${title}</h2>
+    <p>${content}</p>
+    <img src=${image} class="car__content-image" alt="${title}">
+     `;
+}
+
+const menuItem = Array.from(document.querySelectorAll(".menu__item"));
+const removeActiveLink = () => menuItem.forEach((item) => item.classList.remove("active"));
+
 itemList.addEventListener("click", ({target}) => {
     const itemId = target.parentElement.id;
-    const carContent = data.filter(({ title }) => title === itemId);
-    console.log(carContent);
-    const [{ title, content, image }] = carContent;
-    intro.style.display = "none"; 
-    carItem.style.display = "block";
-    carItem.innerHTML = `
-    <h2 style="text-transform:uppercase;">${title}</h2>
-        <p>${content}</p>
-        <img src=${image} class="car__content-image" alt="${title}">
-        `;
+    
+    if (target.parentElement.closest(".menu__item")) {
+        removeActiveLink();
+    
+        if (target.parentElement.classList.contains("active")) {
+            target.parentElement.classList.remove("active");
+        } else {
+            intro.style.display = "none";
+            carItem.style.display = "block";
+            target.parentElement.classList.add("active");
+            carItem.innerHTML = chosenCar(data.find((item) => item.title === itemId));
+        }
+    }
 })
-
