1cbe4802f7f2b9fccf79678e58ef3fc0dcd99e35
Makar Istratov <imakar98@gmail.com>
2023-06-09 00:18:27 +0300

    Created  html template, css and js

---
+++ memory-pair-game/app.js
@@ -0,0 +1,9 @@
+const startGame = () => {
+    const startGameBtn = document.createElement("button")
+    startGameBtn.classList.add("start-game");
+    startGameBtn.innerHTML("Start Game");
+
+    startGameBtn.addEventListener("click", () => {
+        //hide start-menu and generate the board;
+    }); 
+}
---
+++ memory-pair-game/data.js
@@ -0,0 +1,9 @@
+export const iconCards = [
+    'anchor.svg',
+    'grapes.svg',
+    'lemonade.svg',
+    'pineapple.svg',
+    'sea-ball.svg',
+    'shell.svg',
+    'sun.svg'
+];
