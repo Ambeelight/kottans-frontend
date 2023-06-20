18d54c814899403731a65a8f1ddd0030f118b360
Makar Istratov <imakar98@gmail.com>
2023-06-20 04:01:55 +0300

    Added card shaking

--- memory-pair-game/app.js
+++ memory-pair-game/app.js
@@ -1,8 +1,8 @@
 import {iconCards} from './data.js';
 
 const startGameMenu = () => {
-    const startGameBtn = document.createElement("button")
-    const startMenu = document.createElement("div");
+    const startGameBtn = document.createElement("button"),
+     startMenu = document.createElement("div");
 
     startMenu.classList.add("start-menu");
     startGameBtn.classList.add("start-game");
@@ -10,20 +10,16 @@ const startGameMenu = () => {
     document.body.appendChild(startMenu);
     startMenu.appendChild(startGameBtn);
 
-    startGameBtn.addEventListener("click", () => createGame()); 
-}
-
-const timer = document.querySelector(".timer");
-const movesCount = document.querySelector(".moves");
-const stopBtn = document.querySelector(".stop-button");
-
-
+    startGameBtn.addEventListener("click", () => {
+      shuffledCards.forEach((item) => createGame(item[0].slice(0, -4), item[0])); 
+  });
+};
 
 const createGame = (name, item) => {
-  const cardContainer = document.querySelector(".game-field");
-  const cardElement = document.createElement("div");
-  const cardFrontElement = document.createElement("img");
-  const cardBackElement = document.createElement("div");
+  const cardContainer = document.querySelector(".game-field"),
+   cardElement = document.createElement("div"),
+   cardFrontElement = document.createElement("img"),
+   cardBackElement = document.createElement("div");
 
   cardElement.classList.add("card");
   cardElement.dataset.cardIcon = `${name}`;
@@ -57,11 +53,14 @@ const shuffledCards = shuffle(iconCards);
 
 shuffledCards.forEach((item) => createGame(item[0].slice(0, -4), item[0]));
 
+const moveCounter = document.querySelector(".move-counter"); // MC
+let moveCount = 0; // MC
+
 
 //Match the cards
 const cards = document.querySelectorAll('.card');
-let flippedCards = [];
-let matchedCards = [];
+let flippedCards = [],
+ matchedCards = [];
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
@@ -69,17 +68,14 @@ cards.forEach((card) => {
       card.classList.contains("card-flipped") ||
       flippedCards.length === 2 ||
       matchedCards.includes(card)
-    ) {
-      return;
-    }
+    ) return;
 
     card.classList.add("card-flipped");
     flippedCards.push(card);
 
     if (flippedCards.length === 2) {
-      const card1 = flippedCards[0];
-      const card2 = flippedCards[1];
-
+      const card1 = flippedCards[0],
+       card2 = flippedCards[1];
       // Compare cards by data-attributes
       if (card1.dataset.cardIcon === card2.dataset.cardIcon) {
         matchedCards.push(card1, card2);
@@ -92,11 +88,44 @@ cards.forEach((card) => {
         }
       } else {
         setTimeout(() => {
-          card1.classList.remove("card-flipped");
-          card2.classList.remove("card-flipped");
+          flippedCards.forEach((card) => {
+            card.classList.add("shake");
+          })
+        }, 500);
+
+        setTimeout(() => {
+          flippedCards.forEach((card) => {
+            card.classList.remove("shake", "card-flipped");
+          })
           flippedCards = [];
         }, 1000);
-      }
-    }
+      };
+      moveCount++; // MC
+      moveCounter.textContent = `Moves: ${moveCount}`; // MC
+    };
   });
 });
+
+//Timer type??
+const stats = document.querySelector(".stats");
+
+const createTimer = () => {
+  const timerElement = document.createElement('div');
+
+  timerElement.classList.add("timer");
+  stats.appendChild(timerElement);
+
+  if (time > 0) {
+    setInterval(() => {
+      time--;
+      timerElement.textContent = `Time: ${time}`;
+    }, 1000)
+  }
+  
+  return timerElement;
+};
+
+// const moveCounterElement = document.createElement('div');
+//   moveCounterElement.classList.add("move-counter");
+//   stats.appendChild(moveCounterElement);
+//   moveCounterElement.textContent = "Moves: 0"
